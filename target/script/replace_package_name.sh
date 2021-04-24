9fd60c4797332f2a23796d831a2a8b404c7ce801
impactaky <impactaky@users.noreply.github.com>
2021-04-24 13:39:29 +0900

    Add cross compile recipes

--- target/script/replace_package_name.sh
+++ target/script/replace_package_name.sh
@@ -1,5 +1,7 @@
 replace_package_name() {
-    sed -e "s/^gcc\(-[0-9]\+\)\?$/gcc\1-$(arch)-linux-gnu/" \
+    sed -e "s/^binutils$/binutils-$(arch)-linux-gnu/" \
         -e "s/^cpp\(-[0-9]\+\)\?$/cpp\1-$(arch)-linux-gnu/" \
+        -e "s/^g++\(-[0-9]\+\)\?$/g++\1-$(arch)-linux-gnu/" \
+        -e "s/^gcc\(-[0-9]\+\)\?$/gcc\1-$(arch)-linux-gnu/" \
     | sort -u
 }
