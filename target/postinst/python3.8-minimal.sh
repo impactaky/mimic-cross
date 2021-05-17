73e27afc2392d0f541f96ce673607651c01c9007
impactaky <impactaky@users.noreply.github.com>
2021-05-18 00:59:15 +0900

    Fix for find libtorch from cmake

--- target/postinst/python3.8-minimal.sh
+++ target/postinst/python3.8-minimal.sh
@@ -1,8 +1,9 @@
 python3 /mimic-cross/script/save_config_vars.py /mimic-cross/data/config_vars.pickle
 
 mimic-deploy /host/usr/bin/python3.8
-mv /usr/bin/python3.8 /mimic-cross/deploy/host/
-cp /mimic-cross/bin/python3.8 /usr/bin/python3.8
+cp /host/usr/bin/python3.8 /mimic-cross/deploy/host/
+rm /usr/bin/python3.8
+cp /mimic-cross/bin/python3.8 /usr/bin/
 
 # ln -sf /host/usr/lib/python38.zip /usr/lib/python38.zip
 rm -r /usr/lib/python3.8
@@ -12,8 +13,3 @@ cp -r /host/usr/lib/python3.8 /usr/lib/python3.8
 pushd /
 /host/usr/bin/patch -p0 < /mimic-cross/script/sysconfig.patch
 popd
-
-rm -r /usr/local/lib/python3.8/dist-packages
-ln -sf /host/usr/local/lib/python3.8/dist-packages /usr/local/lib/python3.8/dist-packages
-rm -r /usr/lib/python3/dist-packages
-ln -sf /host/usr/lib/python3/dist-packages /usr/lib/python3/dist-packages
