02c5d49f3f3ccc0063ed35274e38df6714ebeb47
impactaky <impactaky@users.noreply.github.com>
2021-10-17 21:42:14 +0900

    python wrapper rename to mimic-python

--- target/postinst/python3.8-minimal.sh
+++ target/postinst/python3.8-minimal.sh
@@ -2,7 +2,7 @@
 mv /usr/bin/python3.8 /mimic-cross/deploy/target/
 mimic-deploy /host/usr/bin/python3.8
 rm -r /usr/bin/python3.8
-cp /mimic-cross/bin/python3.8 /usr/bin/
+cp /mimic-cross/bin/mimic-python /usr/bin/python3.8
 
 python3 /mimic-cross/script/save_config_vars.py /mimic-cross/data/config_vars.pickle
 cp /mimic-cross/data/config_vars.pickle /host/mimic-cross/data
