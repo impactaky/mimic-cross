240149b236b3b7fe854bff28f998a9873b080afe
impactaky <impactaky@users.noreply.github.com>
2023-06-26 16:26:53 +0900

    work for Ubuntu jammy

--- target/postinst/python3-distutils.sh
+++ target/postinst/python3-distutils.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/host/usr/bin/patch --forward /host/usr/lib/python3.8/distutils/sysconfig.py </mimic-cross/script/distutils_sysconfig.patch || [ $? -le 1 ]
+/host/usr/bin/patch --forward /host/usr/lib/python3."$(python3 --version | cut -d "." -f 2)"/distutils/sysconfig.py </mimic-cross/script/distutils_sysconfig.patch || [ $? -le 1 ]
